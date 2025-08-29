d69df3d043375d9819bfc6b57199062852f6ced9
smallyunet <dev.wangyu@gmail.com>
2025-08-29 10:53:17 +0800

    deploy
    Signed-off-by: smallyunet <dev.wangyu@gmail.com>

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,26 +1,24 @@
+// app/.well-known/farcaster.json/route.ts
 import { NextResponse } from 'next/server'
-import { APP_NAME, APP_URL, APP_ICON_URL, APP_SPLASH_URL, APP_SPLASH_BACKGROUND_COLOR, APP_PRIMARY_CATEGORY, APP_TAGS } from '~/lib/constants'
 
 export async function GET() {
   return NextResponse.json({
+    accountAssociation: {
+      header: "eyJmaWQiOjExNjU5OTksInR5cGUiOiJhdXRoIiwia2V5IjoiMHg5REZlOEFiZjFDYjc2RjBBQjNEYjBjM2I1MDgzRTdERjhkNTdBRDZhIn0",
+      payload: "eyJkb21haW4iOiIifQ",
+      signature: "cV2PxtZTKmW0HjHPBaJBBXXbGZKy/eSwVltBHm68ozwnb2DhKzUArtvnqwYbzz1Eu0U3fbCo/r6EWL02XY7XXhs="
+    },
     miniapp: {
       version: '1',
       name: 'Guess 2/3',
-      iconUrl: "https://guessavg.github.io/icon/icon.png",
-      splashImageUrl: "https://guessavg.github.io/icon/icon.png",
+      iconUrl: 'https://guess23.vercel.app/icon.png',
+      splashImageUrl: 'https://guess23.vercel.app/splash.png',
       splashBackgroundColor: '#0b0b10',
       homeUrl: 'https://guess23.vercel.app/',
       requiredChains: ['eip155:8453'],
-      requiredCapabilities: [
-        'wallet.getEthereumProvider'
-      ],
+      requiredCapabilities: ['wallet.getEthereumProvider'],
       primaryCategory: 'games',
       tags: ['game', 'base', 'eth']
-    },
-    accountAssociation: {
-      header: 'eip191:…',
-      payload: '…',
-      signature: '0x…'
     }
   })
 }
